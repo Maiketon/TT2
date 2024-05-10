1d25905c22d630d10c1d6abcb9e3d0677d64e814
Ulises Santos Mendez <ulisesjesus04@gmail.com>
2024-05-10 03:19:15 -0600

    Control de obtencion de rol

--- server/api/controladores/controlEmparejamiento.js
+++ server/api/controladores/controlEmparejamiento.js
@@ -23,3 +23,16 @@ exports.obtenerStrikes = async (req,res) => {
         res.status(500).send('Error en el servidor al obtener el numero de strikes');
     }
 };
+
+exports.obtenerRolEmparejamiento = async (req,res) => {
+    const {userPk} = req.query;
+    try {
+        const respuesta = await modeloEmparejamiento.obtenerRolEmparejamiento(userPk);
+       
+        res.json(respuesta);
+    } catch (err) {
+        console.error('Error realizando la consulta:', err);
+        res.status(500).send('Error en el servidor al obtener el rol');
+    }
+};
+
