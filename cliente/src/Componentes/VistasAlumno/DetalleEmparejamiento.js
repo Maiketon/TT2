cefff878ffce0bf0078e953993ecfb1338912ecd
Ulises Santos Mendez <ulisesjesus04@gmail.com>
2024-05-12 15:37:42 -0600

    Cards dinamicas para mentor y aprendiz

--- cliente/src/Componentes/VistasAlumno/DetalleEmparejamiento.js
+++ cliente/src/Componentes/VistasAlumno/DetalleEmparejamiento.js
@@ -1,85 +1,97 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import { Button, InputGroup, FormControl,Card } from 'react-bootstrap';
 import './Css/EmparejamientoCard.css';
+import perfil_generico from './Utils/perfil.png';
+import axios from "axios";
 
 
 const DetalleEmparejamiento = () => {
+    const userPk = sessionStorage.getItem("userPk");
+    const [Mentor, setMentor] = useState([]);
+    const [Aprendiz, setAprendiz] = useState([]);
+
+
+    useEffect(() => {
+        const obtenerEmparejamientoMentor = async () => {
+            try {
+                const response = await axios.get(`http://localhost:3001/api/emparejamiento/obtenerMentor?userPk=${userPk}`);
+                setMentor(response.data);
+            } catch (error) {
+                console.error('Error al obtener los datos del emparejamiento activo del mentor:', error);
+            }
+        };
+        obtenerEmparejamientoMentor();
+    }, [userPk]);
+
+
+    useEffect(() => {
+        const obtenerEmparejamientoAprendiz = async () => {
+            try{
+                const response = await axios.get(`http://localhost:3001/api/emparejamiento/obtenerAprendiz?userPk=${userPk}`);
+                setAprendiz(response.data);
+            }catch(error){
+                console.error('Error al obtener los datos del emparejamiento activo del aprendiz:', error);
+            }
+        };
+        obtenerEmparejamientoAprendiz();
+    },[userPk]);
+
+
+
     return (
         <div className='margen_superior'>
             <Card>
                 <Card.Body>
                     <Card.Title>Mis mentorias </Card.Title>
-                        <div className="card">
+                    {Mentor.map((mentor, index) => (
+                        <div className="card" key={index}>
                             <div >
                                 <div class="row">
-                                    <div class="col">Foto</div>
-                                    <div class="col">Nombre</div>
+                                    <div class="col"> <img
+                                                    className="img_perfil_m"
+                                                    alt="imagen de perfil"
+                                                    src={perfil_generico}
+                                                />  </div>
+                                    <div class="col">{mentor.nombreCompleto}</div>
                                     <div class="col">Fecha</div>
                                 </div>
                                 <div class="row">
                                     <div class="col"><button className="btn_rechazo">X</button></div>
-                                    <div class="col">ESTADO?</div>
+                                    <div class="col">{mentor.estado}</div>
                                     <div class="col">TOKEN</div>
                                 </div>
                             </div>
-                            
                         </div>
-                        <div className="card">
-                            <div >
-                            <div class="row">
-                                    <div class="col">Foto</div>
-                                    <div class="col">Nombre</div>
-                                    <div class="col">Fecha</div>
-                                </div>
-                                <div class="row">
-                                    <div class="col">BOTON X</div>
-                                    <div class="col">ESTADO?</div>
-                                    <div class="col">TOKEN</div>
-                                </div>
-                            </div>
-                            
-                        </div>
-                        <hr></hr>
-                        <Card.Title>Mis A.O </Card.Title>
-                        <div className="card">
-                            <div >
-                                <div class="row">
-                                    <div class="col">Foto</div>
-                                    <div class="col">Nombre</div>
-                                    <div class="col">Fecha</div>
-                                </div>
-                                <div class="row">
-                                    <div class="col">BOTON X</div>
-                                    <div class="col">ESTADO?</div>
-                                    <div class="col">TOKEN</div>
-                                </div>
-                            </div>
-                            
-                        </div>
-                        <div className="card">
-                            <div >
-                            <div class="row">
-                                    <div class="col">Foto</div>
-                                    <div class="col">Nombre</div>
-                                    <div class="col">Fecha</div>
-                                </div>
-                                <div class="row">
-                                    <div class="col">BOTON X</div>
-                                    <div class="col">ESTADO?</div>
-                                    <div class="col">TOKEN</div>
-                                </div>
-                            </div>
-                            
-                        </div>
+                    ))}
                 </Card.Body>
-                    <div className="conocimientos_deficiencias">
-                        <div className="columna">
-                            <button className="btn_rechazo">X</button>
+            </Card>
+            <p>&emsp;</p>
+           
+            <Card>
+                <Card.Body>
+                    <Card.Title>Mis A.O </Card.Title>
+                    {Aprendiz.map((aprendiz, index) => (
+                        <div className="card" key={index}>
+                            <div >
+                                <div class="row">
+                                    <div class="col"><img
+                                                    className="img_perfil_m"
+                                                    alt="imagen de perfil"
+                                                    src={perfil_generico}
+                                                />  </div>
+                                    <div class="col">{aprendiz.nombreCompleto}</div>
+                                    <div class="col">Fecha</div>
+                                </div>
+                                <div class="row">
+                                    <div class="col"><button className="btn_rechazo">X</button></div>
+                                    <div class="col">{aprendiz.estado}</div>
+                                    <div class="col">TOKEN</div>
+                                </div>
+                            </div>
+                            
                         </div>
-                        <div className="columna">
-                            <button href="#" className="btn btn-primary">Aceptar emparejamento</button>
-                        </div>
-                    </div>
+                    ))}
+                </Card.Body>
             </Card>
         </div>
     );
