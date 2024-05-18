85f699e9fbdeea5a583b13624fdc382008dc3dcc
Maiketon <miky_lee24@hotmail.com>
2024-05-17 21:07:22 -0600

    Prueba con proxy inverso

--- server/servidor.js
+++ server/servidor.js
@@ -8,58 +8,63 @@ const http = require('http');
 
 const app = express();
 
-//ACCESO AL ARCHIVO CON TODOS LOS END POINT DEL ADMINISTRADOR
+// Configuración de CORS para permitir solicitudes desde Netlify
+const corsOptions = {
+  origin: ['https://learnmatch.netlify.app'],
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+// Middleware de bodyParser
+app.use(bodyParser.json());
+
+// Rutas de la API
 const rutasAdministrador = require('./api/rutas/rutasAdministrador');
 app.use('/api/administracion', rutasAdministrador);
 
-
-//ACCESO AL ARCHIVO CON TODOS LOS END POINT DEL ALUMNO
 const rutasAlumnos = require('./api/rutas/rutasAlumnos');
-app.use(bodyParser.json());
 app.use('/api/alumnos', rutasAlumnos);
-//ACCESO AL ARCHIVO CON TODOS LOS ENDPOINT DEL LOGIN
+
 const rutasLogin = require('./api/rutas/rutasLogin');
 app.use('/api/login', rutasLogin);
 
 const rutasAlgoritmo = require('./api/rutas/rutasAlgoritmo');
 app.use('/api/algoritmo', rutasAlgoritmo);
 
-const rutasEmparejamiento =  require('./api/rutas/rutasEmparejamiento');
-app.use('/api/emparejamiento',rutasEmparejamiento);
+const rutasEmparejamiento = require('./api/rutas/rutasEmparejamiento');
+app.use('/api/emparejamiento', rutasEmparejamiento);
 
-  
-  // Servir archivos estáticos de React
-  app.use(express.static(path.join(__dirname, '..', 'cliente', 'build')));
-  // Redirecciona todas las rutas no-API a React
+// Servir archivos estáticos de React
+app.use(express.static(path.join(__dirname, '..', 'cliente', 'build')));
+
+// Redirecciona todas las rutas no-API a React
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'cliente', 'build', 'index.html'));
 });
-  
-//Manejo de errores MiddleWare que se estara desarrollando//
+
+// Manejo de errores 404
 app.use((req, res, next) => {
-    res.status(404).send("La ruta que intentas acceder no existe.");
+  res.status(404).send("La ruta que intentas acceder no existe.");
 });
 
-
+// Manejo de errores generales
 app.use((err, req, res, next) => {
-  console.error(err); // Podrías querer loguear los errores de alguna forma
+  console.error(err);
   res.status(500).send("Ha ocurrido un error en el servidor.");
-
 });
 
-
-
-// Rutas de los certificados SSL
+// Configuración del certificado SSL autofirmado
 const sslOptions = {
   key: fs.readFileSync('C:\\Users\\Anonymous\\Desktop\\TT2\\server\\CertificadosSSL\\private.key'),
   cert: fs.readFileSync('C:\\Users\\Anonymous\\Desktop\\TT2\\server\\CertificadosSSL\\server.crt')
 };
 
-// Puerto en el que deberá escuchar React
+// Puerto HTTPS
 const HTTPS_PORT = 3001;
 https.createServer(sslOptions, app).listen(HTTPS_PORT, () => {
   console.log(`Servidor HTTPS corriendo en el puerto ${HTTPS_PORT}`);
 });
+
 // Redirigir tráfico HTTP a HTTPS
 const HTTP_PORT = 80;
 http.createServer((req, res) => {
