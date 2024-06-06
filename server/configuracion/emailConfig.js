42779093f108502fc9a44516dd9c22c67a33bf6e
Maiketon <miky_lee24@hotmail.com>
2024-06-06 13:33:53 -0600

    Se aumenta el numero de correos que puee realizar el SMTP
    Se aumenta a 15 correos ,como maximo que se puede hacer hacia el SMTP

--- server/configuracion/emailConfig.js
+++ server/configuracion/emailConfig.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
     pool: true, // Habilita el pooling de conexiones
-    maxConnections: 5, // Número máximo de conexiones que se pueden abrir simultáneamente (ajústalo según tus necesidades y los límites del proveedor)
+    maxConnections: 15, // Número máximo de conexiones que se pueden abrir simultáneamente (ajústalo según tus necesidades y los límites del proveedor)
     host: 'smtp-mail.outlook.com', // Servidor SMTP de Outlook
     port: 587, // Puerto estándar para SMTP sobre TLS
     secure: false, // True para 465, false para otros puertos
